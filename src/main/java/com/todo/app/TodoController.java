5ec2cc3e02d2134ff5fa1a2486f0aaac31d187e2
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-09-13 20:55:56 +0530

    feat: Add save method in controller layer.

--- src/main/java/com/todo/app/TodoController.java
+++ src/main/java/com/todo/app/TodoController.java
@@ -1,9 +1,20 @@
 package com.todo.app;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todo")
+@RequiredArgsConstructor
 public class TodoController {
+
+    private final TodoService todoService;
+
+    @PostMapping("/save")
+    public TodoList saveTodo(@RequestBody TodoList todoList) {
+        return todoService.create(todoList);
+    }
 }
